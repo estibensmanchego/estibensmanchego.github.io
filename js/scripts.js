e55a8ce8e535591ee6ea3f17b224dfa9d756b943
Estibens Manchego <emancehgo@idigital.pe>
2014-07-16 18:03:26 -0500

    Cambios Experiencia de trabajo

--- js/scripts.js
+++ js/scripts.js
@@ -44,6 +44,7 @@
         type: 'default GET (Other values: POST)',
         dataType: 'default: Intelligent Guess (Other values: xml, json, script, or html)',
         data: {param1: 'value1'},
+        
     });
     
 
