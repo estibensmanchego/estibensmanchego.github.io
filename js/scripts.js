4c4c111fa6dc6dcac603f85f55b2ba4cdc43b530
Estibens Manchego <emancehgo@idigital.pe>
2014-07-16 16:15:36 -0500

    Cambios Experiencia de trabajo

--- js/scripts.js
+++ js/scripts.js
@@ -39,6 +39,12 @@
     $('.progress .progress-bar').progressbar();
     
     //Sen mail ajax
+    $.ajax({
+        url: '/path/to/file',
+        type: 'default GET (Other values: POST)',
+        dataType: 'default: Intelligent Guess (Other values: xml, json, script, or html)',
+        data: {param1: 'value1'},
+    });
     
 
 
